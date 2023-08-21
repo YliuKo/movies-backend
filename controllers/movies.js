686c087badad1cf269f743f624e155ca7f4231f1
YliuKo <Yliu81@yandex.ru>
2023-08-22 02:10:08 +0300

    l

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,13 +1,13 @@
-const movieSchema = require('../models/movie');
-const Forbidden = require('../errors/forbidden');
-const NotFound = require('../errors/notFound');
-const BadRequest = require('../errors/badRequest');
+const movieSchema = require("../models/movie");
+const Forbidden = require("../errors/forbidden");
+const NotFound = require("../errors/notFound");
+const BadRequest = require("../errors/badRequest");
 const {
   movieNotFoundText,
   movieIdNotFoundText,
   BadRequestText,
   movieForbiddenText,
-} = require('../errors/errorText');
+} = require("../errors/errorText");
 
 // получаем movie
 module.exports.getMovie = (req, res, next) => {
@@ -24,12 +24,13 @@ module.exports.getMovie = (req, res, next) => {
 
 // создаем movie
 module.exports.addMovie = (req, res, next) => {
-  movieSchema.create({ ...req.body, owner: req.user._id })
+  movieSchema
+    .create({ ...req.body, owner: req.user._id })
     .then((movie) => {
       res.status(201).send(movie);
     })
     .catch((error) => {
-      if (error.name === 'ValidationError') {
+      if (error.name === "ValidationError") {
         next(new BadRequest(BadRequestText));
       } else {
         next(error);
@@ -39,24 +40,26 @@ module.exports.addMovie = (req, res, next) => {
 
 // удаляем movie
 module.exports.deleteMovie = (req, res, next) => {
-  const { movieId } = req.params;
+  const { movieId: id } = req.params;
 
-  movieSchema.findById({ _id: movieId })
+  movieSchema
+    .findById({ movieId: id })
     .then((movie) => {
       if (!movie) {
         throw new NotFound(movieIdNotFoundText);
       } else if (!movie.owner.equals(req.user._id)) {
         throw new Forbidden(movieForbiddenText);
       } else {
-        movie.deleteOne()
-          .then(() => res.status(200).send({ message: 'Фильм удален.' }))
+        movie
+          .deleteOne()
+          .then(() => res.status(200).send({ message: "Фильм удален." }))
           .catch((error) => {
             next(error);
           });
       }
     })
     .catch((error) => {
-      if (error.name === 'CastError') {
+      if (error.name === "CastError") {
         next(new BadRequest(BadRequestText));
       } else {
         next(error);
