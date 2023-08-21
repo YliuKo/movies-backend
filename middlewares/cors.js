fb2ed064e787fa2d2b0b3f1da9b5746364d16e68
YliuKo <Yliu81@yandex.ru>
2023-08-22 02:42:24 +0300

    l

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,30 +1,30 @@
 const allowedCors = [
-  'https://movies.yuiko.nomoreparties.co',
-  'http://movies.yuiko.nomoreparties.co',
-  'https://api.movies.yuiko.nomoreparties.co',
-  'http://api.movies.yuiko.nomoreparties.co',
-  'localhost:3000',
-  'http://localhost:3000',
-  'https://localhost:3000',
-  'localhost:3001',
-  'http://localhost:3001',
+  "https://movies.yuiko.nomoredomainsicu.ru",
+  "http://movies.yuiko.nomoredomainsicu.ru",
+  "https://api.movies.yuiko.nomoreparties.co",
+  "http://api.movies.yuiko.nomoreparties.co",
+  "localhost:3000",
+  "http://localhost:3000",
+  "https://localhost:3000",
+  "localhost:3001",
+  "http://localhost:3001",
 ];
 
-const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
+const DEFAULT_ALLOWED_METHODS = "GET, HEAD, PUT, PATCH, POST, DELETE";
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
+  const requestHeaders = req.headers["access-control-request-headers"];
 
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Credentials", true);
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Origin", origin);
   }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+  if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
     return res.end();
   }
 
